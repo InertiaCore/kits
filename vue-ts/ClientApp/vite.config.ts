4f7479fc923e7ec4a9e2dd60be18e34193ae9bfa
Austin Drummond <adrum0x7@gmail.com>
2025-02-15 23:40:27 -0500

    added vue example

---
+++ vue-ts/ClientApp/vite.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from "vite";
+import vue from '@vitejs/plugin-vue';
+import laravel from "laravel-vite-plugin";
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+    plugins: [
+        tailwindcss(),
+        laravel({
+            input: ["src/App.vue"],
+            publicDirectory: "../wwwroot",
+            hotFile: "../wwwroot/build/hot",
+            refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+    ],
+    resolve: {
+        alias: {
+            "@": "/src",
+        },
+    },
+    build: {
+        emptyOutDir: true,
+    },
+});
